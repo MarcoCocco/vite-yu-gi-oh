fd2625d93aa79ce8b370aefd812adff5e4c15442
MarcoCocco <marcococco28@gmail.com>
2023-03-28 19:39:59 +0200

    feat:  aggiunto il titolo e il tipo di carta

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -21,7 +21,12 @@ export default {
     <div id="card">
         <div class="card-image">
             <img :src="card.card_images[0].image_url" alt="">
-            
+        </div>
+        <div class="title-card">
+            <p><strong>{{ card.name }}</strong></p>
+        </div>
+        <div class="card-type">
+            <p>{{ card.type }}</p>
         </div>
     </div>
 </template>
@@ -29,10 +34,18 @@ export default {
 <style lang="scss" scoped>
 #card {
     width: calc(100% / 5 - 30px);
+    margin-bottom: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
     img {
         width: 100%;
     }
-}
 
+    .title-card {
+        font-size: 1.4em;
+        text-align: center;
+    }
+}
 </style>
